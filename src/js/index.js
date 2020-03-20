5670f046872043eb5857955dbe66246656d8cdc8
Anton Voitenko <voytenko.ad@yandex.ru>
2020-03-21 01:00:31 +0800

    Ops, fixed truble with heder max width

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // styles
 import '../pages/index.css';
+
+// Toggle mobile menu
+
+const toggleMobileMenu = () => {
+  document.querySelector('.header__hamburger').classList.toggle('header__hamburger_open');
+  document.querySelector('.header').classList.toggle('header_hamburger-is-active');
+  document.querySelector('.menu').classList.toggle('menu_is-open');
+  document.querySelector('.page').classList.toggle('page__overflow');
+};
+
+document.querySelector('.header__hamburger').addEventListener('click', () => {
+  toggleMobileMenu();
+});
