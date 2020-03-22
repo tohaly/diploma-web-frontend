d2c25ad0ca9188e68bff9c5f6d6ebe0ed5c035b8
Anton Voitenko <voytenko.ad@yandex.ru>
2020-03-23 00:37:31 +0800

    Complited second page

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,7 @@ import '../pages/index.css';
 const toggleMobileMenu = () => {
   document.querySelector('.menu').classList.toggle('menu_is-open');
   document.querySelector('.page').classList.toggle('page__overflow');
-
-  if (window.location.pathname === '/index.html') {
+  if (window.location.pathname !== '/saved-news.html') {
     document.querySelector('.header').classList.toggle('header_hamburger-is-active');
     document.querySelector('.header__hamburger').classList.toggle('header__hamburger_open');
   } else {
@@ -19,4 +18,42 @@ const toggleMobileMenu = () => {
 
 document.querySelector('.header__hamburger').addEventListener('click', () => {
   toggleMobileMenu();
+});
+
+// delete card
+
+let timer;
+
+document.addEventListener('click', () => {
+  if (event.target.classList.contains('card__button')) {
+    event.target.nextElementSibling.classList.toggle('card__confirm_is-active');
+  }
+});
+
+document.addEventListener('mouseout', event => {
+  if (event.target.classList.contains('card__button')) {
+    timer = setTimeout(() => {
+      event.target.nextElementSibling.classList.remove('card__confirm_is-active');
+    }, 1000);
+  }
+  if (event.target.classList.contains('card__confirm')) {
+    timer = setTimeout(() => {
+      event.target.classList.remove('card__confirm_is-active');
+    }, 1000);
+  }
+});
+
+document.addEventListener('mouseover', event => {
+  if (
+    event.target.classList.contains('card__confirm') ||
+    event.target.classList.contains('card__button')
+  ) {
+    clearTimeout(timer);
+  }
+});
+
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('card__confirm_delete')) {
+    alert('Новость успешно удалена');
+  }
 });
